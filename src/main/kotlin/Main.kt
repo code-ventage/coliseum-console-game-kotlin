d9019f70931810460392ab95d8efb44e00ba0101
Victor Palmero <victor.palmero@undostres.com.mx>
2024-07-01 12:28:21 -0400

    feat: delete hello world

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,6 @@ package com.palmerodev
 import com.palmerodev.services.FightService
 
 fun main() {
-    println("Hello World!")
     val fightService = FightService()
     fightService.initialize()
 }
